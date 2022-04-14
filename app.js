3219e748d376d567a9f31b19371e8e952a89dbe0
TWALHA <elasmicalmarwayn@gmail.com>
2022-04-14 12:53:03 +0300

    Addition of databases

--- app.js
+++ app.js
@@ -1,44 +1,143 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
+const  mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express()
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
+                  
+const itermsSchema = new mongoose.Schema({
+    name: String
+});
 
-let iterms = [];
-let Workiterms = [];
+const iterm = mongoose.model("iterm",itermsSchema);
+
+const iterm1 = new iterm({
+    name: "Welcome to your to do list!"
+});
+
+const iterm2 = new iterm({
+    name: "Hit the + button to add a new iterm"
+});
+
+const iterm3 = new iterm({
+    name: "<-- Hit this to delete an iterm"
+});
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    iterms: [itermsSchema]
+});
+
+const List = new mongoose.model("List",listSchema);
+
+const defaultIterms = [iterm1,iterm2,iterm3]
 
 app.get("/",function(req,res){
+
+    iterm.find({},function(err,foundIterms){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundIterms.length===0){
+                iterm.insertMany(defaultIterms,function(err){
+                    if (err){
+                        console.log(err);
+                    }else{
+                        console.log("Succesfully saved the 3 iterms");
+                    }
+                });
+                
+                res.redirect("/");
+            }
+            else{
+                res.render("lists",{listTitle:"Today", newListIterms: foundIterms});
+            }
+        }
+    });  
+});
+
+app.get("/:customListName",function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName},function(err,foundList){
+        if(!err){
+            if(!foundList){
+                //creates new list
+                const list = new List({
+                    name: customListName,
+                    iterms: defaultIterms
+                });
+            
+                list.save();
+
+                res.redirect("/" + customListName);
+            }else{
+                //shows existing lists
+                res.render("lists",{listTitle: foundList.name, newListIterms: foundList.iterms});
+            }
+        }
+    })
+
     
-    let day = date.getDate();
-    res.render("lists",{listTitle:day, newListIterms: iterms});
+});
 
-})
-
-app.get("/work",function(req,res){
-
-    res.render("lists", {listTitle: "Work List", newListIterms: Workiterms})
-})
 
 app.get("/about",function(req,res){
     res.render("about")
-})
+});
 
 app.post("/",function(req,res){
-    let iterm = req.body.newiterm;
+    const itermName = req.body.newiterm;
+    const listName = req.body.list;
 
-    if (req.body.list === "Work"){
-        Workiterms.push(iterm);
-        res.redirect("/work");
-    }else{
-        iterms.push(iterm);
+    const iterm4 = new iterm({
+        name: itermName
+    });
+
+    
+    if(listName==="Today"){
+        iterm4.save();
         res.redirect("/");
-    }  
-})
+    }else{
+        List.findOne({name: listName},function(err,foundList){
+            if(!err){
+                foundList.iterms.push(iterm4);
+                foundList.save();
+                res.redirect("/" + listName);
+            }
+        });
+    }
+});
+
+app.post("/delete",function(req,res){
+    const checkedItermId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if(listName === "Today"){
+        iterm.findByIdAndRemove(checkedItermId,function(err){
+            if(err){
+                console.log(err)
+            }else{
+                console.log("successfully deleted!")
+            }
+        });
+    
+        res.redirect("/");
+    }else{
+        List.findOneAndUpdate({name: listName},{$pull: {iterms: {_id: checkedItermId}}},function(err,foundList){
+        
+            if(!err){
+                res.redirect("/" + listName);
+            }
+        });
+    }
+
+});
 
 app.listen(3000,function(){
     console.log("SERVER HAS STARTED AT PORT 3000")
